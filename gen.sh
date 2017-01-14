8eca022f197621767a916e52acb2be4d260ec3dd
Elta <elta.era@gmail.com>
2017-01-14 00:48:20 -0500

    M .ci.msg  M gen.sh

--- gen.sh
+++ gen.sh
@@ -5,7 +5,7 @@ CI_MSG=.ci.msg
 function commitAll()
 {
 	git st -s        > ${CI_MSG}
-	sed -i "1,${DATA}" ${CI_MSG}
+	echo "${DATA}"  >> ${CI_MSG}
     git st -s | awk '{ print $2 }' | xargs git add
 	git commit -F  ${CI_MSG}
 	rm -rf ${CI_MSG}
