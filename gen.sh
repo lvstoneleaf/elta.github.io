7094da380ec01fc93f12bbf4ca49aa6b851ab710
Elta <elta.era@gmail.com>
2017-01-14 01:04:40 -0500

    D .gen.sh.swp  M gen.sh

--- gen.sh
+++ gen.sh
@@ -22,16 +22,20 @@ case $1 in
 		echo "All step"
 		$0 ci
 		$0 push
+		exit 0
 		;;
 	ci)
 		echo "Commit all"
 		commitAll
+		exit 0
 		;;
 	push)
 		echo "Push"
 		$0 pushRepo
+		exit 0
 		;;
 	*)
 		$0 all
+		exit 0
 		;;
 esac
